79a83880df248b107fc91240498641744b7b1f21
ShonBC <cortesshon@gmail.com>
2021-11-27 11:04:28 -0800

    B2.17 Init bash script to run cpplint

---
+++ run_cpplint.sh
@@ -0,0 +1,2 @@
+cpplint --verbose 5 $( find . -name *.cpp | grep -vE -e "^./build/" -e "^./vendor/") $( find . -name *.h | grep -vE -e "^./build/" -e "^./vendor/") > results/cpplint_result.txt
+echo "Done Processing. Results are stored in results/cpplint_result.txt"

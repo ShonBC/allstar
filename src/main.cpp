b9e905ca664895249df3bdca1cac366ed564fbf8
pooja-kabra <pkabra@terpmail.umd.edu>
2021-12-05 09:48:09 -0500

    B2.14 Call AssignGoals() in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "../include/swarm_server.h"
+#include <vector>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "walker");
+
+    std::vector<std::vector<double>> input = {
+        {1,1},
+        {3,4},
+        {5,5},
+        {4,8}
+    };
     SwarmServer swarm;
-    swarm.InitiateMap();
+    // swarm.InitiateMap();
+    swarm.AssignGoals(input);
 }

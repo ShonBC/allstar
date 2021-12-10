9763d6336a4f34df23a43f086ace669dea45fa99
ShonBC <cortesshon@gmail.com>
2021-12-10 13:13:53 -0800

    3.18 Update to run on ROS "main" executable

--- run_valgrind.sh
+++ run_valgrind.sh
@@ -1,3 +1,3 @@
-cd build 
-valgrind --leak-check=full --log-file=../results/valgrind_results.txt ./app/shell-app
+cd ../../devel/lib/allstar/
+valgrind --leak-check=full --log-file=../../../src/allstar/results/valgrind_results.txt ./main 4 ../../../src/allstar/src/test4.jpg
 echo "Done Processing. Results are stored in results/valgrind_results.txt"

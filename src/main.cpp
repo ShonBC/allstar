8ed75b940ce2848f367ff1f2c3f369732256ff39
Sameer Pusegaonkar <sameer@umd.edu>
2021-12-09 21:24:23 -0500

    3.36 Change the main to test the entire program

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,29 @@
 #include "../include/swarm_server.h"
 #include "../include/image_processor.h"
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "walker");
+// int main(int argc, char **argv) {
+//   ros::init(argc, argv, "walker");
 
-  std::vector<std::vector<double>> input = {
-    {-1, -1},
-    {13, 13},
-    {25, 25},
-    {34, 34}
-  };
-  SwarmServer swarm;
-  // swarm.InitiateMap();
-  swarm.AssignGoals(input);
-}
-// int main(int argc, char** argv) {
-//   ros::init(argc, argv, "main");
-//   cv::Mat image = cv::imread("/home/sameer/catkin_ws/src/allstar/src/test.jpg");
-//   auto img = new ImageProcessor(image);
-//   auto countours = img->GetEdges();
-//   img->RefineGoalPoints(4, countours);
-//   auto points = img->GetGoalPoints();
-//   ROS_INFO_STREAM("Got " << points.size() << " goal points!");
-//   // SwarmServer swarm;
-//   // swarm.AssignGoals(points);
-//   ROS_INFO_STREAM("Finished assigning points!");
+//   std::vector<std::vector<double>> input = {
+//     {1, 1},
+//     {3, 3}
+//     // {5, 5}
+//     // {34, 34}
+//   };
+//   SwarmServer swarm;
+//   // swarm.InitiateMap();
+//   swarm.AssignGoals(input);
 // }
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "main");
+  cv::Mat image = cv::imread("/home/sameer/catkin_ws/src/allstar/src/test.jpg");
+  auto img = new ImageProcessor(image);
+  auto countours = img->GetEdges();
+  img->RefineGoalPoints(3, countours);
+  // auto points = img->GetGoalPoints();
+  auto points = img->TransformToMapCoordinates();
+  ROS_INFO_STREAM("Got " << points.size() << " goal points!");
+  SwarmServer swarm;
+  swarm.AssignGoals(points);
+  ROS_INFO_STREAM("Finished assigning points!");
+}

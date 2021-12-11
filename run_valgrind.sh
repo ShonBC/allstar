22f4851e3abfe0592e3dee2d96a117f5f6ec5af1
ShonBC <cortesshon@gmail.com>
2021-12-11 11:23:44 -0800

    3.3 Fix scrript to run roscore

--- run_valgrind.sh
+++ run_valgrind.sh
@@ -1,4 +1,4 @@
-gnome-terminal -- "roscore; bash"
+gnome-terminal -x sh -c  "roscore; bash"
 cd ../../devel/lib/allstar/
 valgrind --leak-check=full --log-file=../../../src/allstar/results/valgrind_results.txt ./main 4 ../../../src/allstar/src/test4.jpg
 echo "Done Processing. Results are stored in results/valgrind_results.txt"

09f6b93016093727b82f937632f9fc2ecae6064d
ShonBC <cortesshon@gmail.com>
2021-12-10 18:06:17 -0800

    3.45 Adapt to new improved refine goal points node

--- src/improved_main.cpp
+++ src/improved_main.cpp
@@ -11,8 +11,16 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "main");
   cv::Mat image = cv::imread(argv[2]);
   auto img = new ImageProcessor(image);
-  auto bin = img->GetEdges();
-  img->RefineGoalPoints(std::atoi(argv[1]), bin);
+
+  cv::Mat bw_img;
+  cv::Mat bin;
+
+  cv::cvtColor(image, bw_img, cv::COLOR_BGR2GRAY);
+  cv::threshold(bw_img, bin, 100, 255, cv::THRESH_BINARY_INV);
+  cv::imshow("bin", bin);
+  cv::waitKey(0);
+  img->ImprovedRefineGoalPoints(std::atoi(argv[1]), bin);
+
   auto points = img->TransformToMapCoordinates();
   ROS_INFO_STREAM("Got " << points.size() << " goal points!");
   SwarmServer swarm;

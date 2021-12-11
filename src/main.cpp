8334ea0e13ab42f93ffe78dfe5ce4724b64bfb9f
ShonBC <cortesshon@gmail.com>
2021-12-10 18:44:04 -0800

    3.3 Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ int main(int argc, char** argv) {
   auto img = new ImageProcessor(image);
   auto bin = img->GetEdges();
   img->RefineGoalPoints(std::atoi(argv[1]), bin);
-  // img->ImprovedRefineGoalPoints(std::atoi(argv[1]), bin);
 
   auto points = img->TransformToMapCoordinates();
   ROS_INFO_STREAM("Got " << points.size() << " goal points!");
@@ -22,36 +21,3 @@ int main(int argc, char** argv) {
   ROS_INFO_STREAM("Kernel size: " << img->GetKernalSize());
   swarm.AssignGoals(points);
 }
-
-
-// int main(int argc, char** argv) {
-//     if (argc <= 1) {
-//     ROS_WARN_STREAM
-//     ("Enter the # of Robots (robots_) and absolute Image File Path.");
-//     return 1;
-//     }
-
-//   ros::init(argc, argv, "main");
-//   cv::Mat image = cv::imread(argv[2]);
-//   auto img = new ImageProcessor(image);
-
-//   cv::Mat bw_img;
-//   cv::Mat bin;
-
-//   cv::cvtColor(image, bw_img, cv::COLOR_BGR2GRAY);
-//   cv::threshold(bw_img, bin, 100, 255, cv::THRESH_BINARY_INV);
-//   cv::imshow("bin", bin);
-//   img->ImprovedRefineGoalPoints(std::atoi(argv[1]), bin);
-
-//   auto points = img->TransformToMapCoordinates();
-//   ROS_INFO_STREAM("Got " << points.size() << " goal points!");
-
-//   for ( auto i  = 0; i < points.size(); i++ ) {
-//     ROS_INFO_STREAM("goal points: " << points[i][0] << " " << points[i][1]);
-//   }
-
-//   SwarmServer swarm;
-//   swarm.num_agents = std::atoi(argv[1]);
-//   ROS_INFO_STREAM("Kernel size: " << img->GetKernalSize());
-//   swarm.AssignGoals(points);
-// }
